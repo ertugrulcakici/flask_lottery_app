3ab2f11ad73c24b8348da0b46a5a770ecbe22e23
Ertuğrul Çakıcı <72172226+ertugrulcakici@users.noreply.github.com>
2023-07-17 17:44:00 +0300

    Converted to English

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
 from random import choice
+
 from flask import Flask, render_template, request
+
 app = Flask(__name__)
 
-@app.route('/',methods=["GET","POST"])
+
+@app.route('/', methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         data = request.form.get("additems").split("\n")
         newdata = dict()
-        newdata["kazanan"] = choice(data)
-        data.remove(newdata["kazanan"])
-        newdata["yedek"] = choice(data)
-        return render_template("index.html",willadd=False,data=newdata)
+        newdata["winner"] = choice(data)
+        data.remove(newdata["winner"])
+        newdata["Subsitute"] = choice(data)
+        return render_template("index.html", willadd=False, data=newdata)
     else:
-        return render_template("index.html",willadd=True)
+        return render_template("index.html", willadd=True)
 
-if __name__=="__main__":
-    app.run(debug=True,host="192.168.1.32",port=80)
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080)
